da3440278dd6c262d3ddfab99878975621363710
ccwyn <ccwyn93@163.com>
2020-09-11 17:52:29 +0800

    init

---
+++ pages/about.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    s
+  </div>
+</template>
+
+<script>
+
+  export default {
+    name:'AboutPage',
+    props:[''],
+    data () {
+      return {
+
+      };
+    },
+
+    components: {},
+
+    computed: {},
+
+    beforeMount() {},
+
+    mounted() {},
+
+    methods: {},
+
+    watch: {}
+
+  }
+
+</script>
+<style lang='' scoped>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>hell nuxt js</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomePage",
+  props: [""],
+  data() {
+    return {};
+  },
+
+  components: {},
+
+  computed: {},
+
+  beforeMount() {},
+
+  mounted() {},
+
+  methods: {},
+
+  watch: {},
+};
+</script>
+<style lang='' scoped>
+</style>
