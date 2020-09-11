34596333ad97d44dfa613e6332f6928b345f65f0
ccwyn <ccwyn93@163.com>
2020-09-11 19:01:07 +0800

    ..

---
+++ pages/user/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>user page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "userPage",
+  props: [""],
+  data() {
+    return {};
+  },
+
+  components: {},
+
+  computed: {},
+
+  beforeMount() {},
+
+  mounted() {},
+
+  methods: {},
+
+  watch: {},
+};
+</script>
+<style lang='' scoped>
+</style>
