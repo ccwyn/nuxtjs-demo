6dc0136152e45a9991f4c7edf1ccae56c2693993
ccwyn <ccwyn93@163.com>
2020-09-18 15:04:15 +0800

    fu lu you

---
+++ pages/users.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1>hell nuxt js</h1>
+    <nuxt-child/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "UsersPage",
+  props: [""],
+  data() {
+    return {};
+  },
+
+  components: {},
+
+  computed: {},
+
+  beforeMount() {},
+
+  mounted() {},
+
+  methods: {},
+
+  watch: {},
+};
+</script>
+<style lang='' scoped>
+</style>
---
+++ pages/users/foo.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>foo page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "userFoo",
+  props: [""],
+  data() {
+    return {};
+  },
+
+  components: {},
+
+  computed: {},
+
+  beforeMount() {},
+
+  mounted() {},
+
+  methods: {},
+
+  watch: {},
+};
+</script>
+<style lang='' scoped>
+</style>
