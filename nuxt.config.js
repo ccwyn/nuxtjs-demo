866ac95d561810f87e6a65c7d7cd4754bdf6d843
ccwyn <ccwyn93@163.com>
2020-09-18 15:26:05 +0800

    config

---
+++ nuxt.config.js
@@ -0,0 +1,12 @@
+module.exports={
+  router:{
+    base:'/abc',
+    extendRoutes(routes,resolve){
+      routes.push({
+        path:'/hello',
+        name:'hello',
+        component:resolve(__dirname,'pages/about.vue')
+      })
+    }
+  }
+}
